e5d2c77e2f4c771ba2161fccef72e775c5ed2104
Islombek Abdurahmonov <113435241+221236cs09@users.noreply.github.com>
2022-11-22 11:20:03 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  problem2
+//
+//  Created by Islombek Abdurahmonov on 22/11/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    int g1, g2;
+    cin >> g1 >> g2;
+    return 0;
+}
